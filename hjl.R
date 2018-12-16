d85deffc5fd5581f1d20fd785fb26138dd64c428
Tex <texchi2@gmail.com>
2018-12-16 23:00:51 +0800

    Update hjl.R

--- hjl.R
+++ hjl.R
@@ -1,2 +1,3 @@
 # ?? git config  credential.helper osxkeychain
 # https://jennybc.github.io/2014-05-12-ubc/ubc-r/session03_git.html
+# https://www.r-bloggers.com/rstudio-pushing-to-github-with-ssh-authentication/
