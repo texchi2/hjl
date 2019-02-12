756be180fe168070998bde61b53fde19b13c22a9
texchi2 <texchi2@gmail.com>
2019-02-12 23:32:30 +0000

    sync

--- hjl.R
+++ hjl.R
@@ -1,7 +1,19 @@
-# ?? git config  credential.helper osxkeychain
+# #bash-4.2$ git config  credential.helper osxkeychain
 # https://jennybc.github.io/2014-05-12-ubc/ubc-r/session03_git.html
 # https://www.r-bloggers.com/rstudio-pushing-to-github-with-ssh-authentication/
-# # hjl 
+# {
+# bash-4.2$ git config  user.email "texchi2@gmail.com"
+# bash-4.2$ git config  user.name "texchi2"
+# x bash-4.2$ git config --global credential.helper osxkeychain
+# warning: unable to access '/home/rstudio/.gitconfig': Permission denied
+# error: opening /home/rstudio/.gitconfig: Permission denied
+# bash-4.2$ git config  credential.helper osxkeychain
+# bash-4.2$ git config remote.origin.url git@github.com:texchi2/hjl.git
+# bash-4.2$ chmod 400 /home/rstudio/.ssh/id_rsa
+# }
+#
+#
+# hjl 
 # Rstudio for mass spectrum analysis t0 t1 t2 by protein/peptide drug
 # treatment => "phosphorylation signalling" fold change: t1/t0, t2/t0 
 # load IA_final_protein.csv into data.frame
